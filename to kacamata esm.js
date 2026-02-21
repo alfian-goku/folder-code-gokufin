e3dabc88800d20c58f8874e0cef2e6e75a30e702
alfian-goku <sundaycamp23@gmail.com>
2026-02-21 09:00:05 +0800

    Create to kacamata esm.js

---
+++ to kacamata esm.js
@@ -0,0 +1,117 @@
+/*
+Name Fitur : to kacamata type esm
+Type Code : esm
+Base Api : api.faa
+Created By : alfian
+*/
+
+import axios from "axios";
+import fs from "fs-extra";
+import path from "path";
+import FormData from "form-data";
+import { downloadMedia, downloadQuotedMedia, reply } from "../../lib/utils.js"; // ini buat download media dari pengguna
+
+/* UPLOAD AUTORESBOT */
+async function uploadAutoresbot(filePath) {
+  try {
+    const form = new FormData();
+    form.append("file", fs.createReadStream(filePath));
+
+    const res = await axios.put(
+      "https://autoresbot.com/tmp-files/upload",
+      form,
+      { headers: form.getHeaders() }
+    );
+
+    return res.data?.data?.url||false;
+  } catch {
+    return false;
+  }
+}
+
+async function handle(sock, messageInfo) {
+  const {
+    m,
+    message,
+    remoteJid,
+    isQuoted,
+    type,
+    prefix,
+    command
+  } = messageInfo;
+
+  try {
+    const mediaType = isQuoted ? isQuoted.type : type;
+
+    if (mediaType !== "image") {
+      return reply(
+        m,
+`⚠️ *Format Penggunaan:*
+
+🖼️ Kirim / reply gambar dengan caption:
+*${prefix + command}*`
+      );
+    }
+
+    // 🕶️ reaction loading
+    await sock.sendMessage(remoteJid, {
+      react: { text: "🕶️", key: message.key }
+    });
+
+    // download image
+    const media = isQuoted
+      ? await downloadQuotedMedia(message)
+      : await downloadMedia(message);
+
+    const mediaPath = path.join("tmp", media);
+    if (!fs.existsSync(mediaPath)) throw "File tidak ditemukan";
+
+    // upload image
+    const imageUrl = await uploadAutoresbot(mediaPath);
+    fs.unlinkSync(mediaPath);
+
+    if (!imageUrl) throw "Upload gagal";
+
+    // call API FAA
+    const apiUrl =
+      `https://api-faa.my.id/faa/tokacamata?url=${encodeURIComponent(imageUrl)}`;
+
+    // kirim hasil
+    await sock.sendMessage(
+      remoteJid,
+      {
+        image: { url: apiUrl },
+        caption: "🕶️ *Kacamata berhasil ditambahkan*"
+      },
+      { quoted: message }
+    );
+
+    // ✅ reaction selesai
+    await sock.sendMessage(remoteJid, {
+      react: { text: "✅", key: message.key }
+    });
+
+  } catch (err) {
+    console.error("To kacamata error:", err?.response?.data || err.message);
+
+     // Kirim pemberitahuan ke user
+    await sock.sendMessage(
+      remoteJid,
+      { text: '⚠️ Terjadi keselahan saat memproses gambar!' },
+      { quoted: message }
+    );
+
+    // reaction eror
+    await sock.sendMessage(remoteJid, {
+      react: { text: "❌", key: message.key }
+    });
+  }
+}
+
+export default {
+  handle,
+  Commands: ["tokacamata", "kacamata"],
+  OnlyPremium: false,
+  OnlyOwner: false,
+  limitDeduction: 1
+};
