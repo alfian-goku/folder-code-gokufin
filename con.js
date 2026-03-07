faa9c53fb36c035f8163e0181df267ff23040b85
𝗔𝗹𝗳𝗶𝗮𝗻 <sundaycamp23@gmail.com>
2026-03-07 18:17:48 +0800

    delete con.js

--- con.js
+++
@@ -1,7 +0,0 @@
-✧  *G E T / F E T C H*
-
-    ◦  *URL* : https://art-global.faceai.art/aifaceswap-demo/gen-temp/2026-3-7/raw_192a691c36bf5c602e6c7838dab4bc78.png
-    ◦  *Status* : ```200```
-    ◦  *Size* : ```1.71 MB```
-    ◦  *Mime* : ```image/png```
-    ◦  *Output* : ```png```
