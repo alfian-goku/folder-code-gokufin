06227fc39c796a22929116ed7bf8f906d72d9983
𝗔𝗹𝗳𝗶𝗮𝗻 <sundaycamp23@gmail.com>
2026-03-09 22:43:44 +0800

    delete testing.js

--- testing.js
+++
@@ -1,1 +0,0 @@
-Isi nama file / judul file yg ku lingkari , setiap code pakein *.js* udh lu tinggal paste code di situ ( warna merah ) udh? Klik commit change lalu klik lagi kek gitu "commit" atau buat
